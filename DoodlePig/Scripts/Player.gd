0f6510f603e201fbab2bdd06d4c1b8757ad5592e
Quentin <quentin@pop-os.localdomain>
2020-09-22 10:51:02 +0200

    add the bouciness things to the player

--- DoodlePig/Scripts/Player.gd
+++ DoodlePig/Scripts/Player.gd
@@ -1,7 +1,11 @@
 extends RigidBody2D
 
+var jump_speed = 600
+
 func _ready():
 	pass
 
 func collision(body):
+	if body.is_in_group('paddles'):
+		set_linear_velocity(Vector2(0,-jump_speed));
 	pass
