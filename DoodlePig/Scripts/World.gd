b2b6a79fb1544d23541f86af6089de618427c1fe
Hausmann <william.hausmann@cpnv.ch>
2020-09-22 18:20:21 +0200

    add moving clouds

--- DoodlePig/Scripts/World.gd
+++ DoodlePig/Scripts/World.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-var platform = preload("res://Instances/CloudPlatform.tscn")
+
+var cloud_platform = preload("res://Scripts/CloudPlatform.tscn")
+var movingcloud_platform = preload("res://Scripts/MovingCloudPlatform.tscn") 
+
+
 var width
 var offset_x = 40
 
@@ -9,12 +13,16 @@ func _ready():
 	pass
 	
 func generateCloud(y):
+	var new_platform
 	width = get_viewport_rect().size.x
 	randomize()
 	for i in range(0,30):
-		var new_platform = platform.instance()
+		var rand = rand_range(0,100)
+		if rand >= 0 and rand <20 :
+			new_platform = movingcloud_platform.instance()
+		else:
+			new_platform = cloud_platform.instance()
 		new_platform.set_position(Vector2(rand_range(-width/2 + offset_x,width/2 - offset_x),y))
 		add_child(new_platform)
-		y -= rand_range(30,130)
-	pass
+		y -= rand_range(50,200)
 
