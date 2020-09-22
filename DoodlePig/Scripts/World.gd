49b4d8ee90bd382afcd4bffb18bd1a7e0e0bbe74
Hausmann <william.hausmann@cpnv.ch>
2020-09-22 16:08:54 +0200

    add offset & parmater

--- DoodlePig/Scripts/World.gd
+++ DoodlePig/Scripts/World.gd
@@ -2,18 +2,19 @@ extends Node2D
 
 var platform = preload("res://Instances/CloudPlatform.tscn")
 var width
+var offset_x = 40
 
 func _ready():
-	generateCloud()
+	generateCloud(0)
 	pass
 	
-func generateCloud():
+func generateCloud(y):
 	width = get_viewport_rect().size.x
 	randomize()
-	var y = 0
 	for i in range(0,30):
 		var new_platform = platform.instance()
-		new_platform.set_position(Vector2(rand_range(-width/2,width/2),y))
+		new_platform.set_position(Vector2(rand_range(-width/2 + offset_x,width/2 - offset_x),y))
 		add_child(new_platform)
 		y -= rand_range(30,130)
 	pass
+
