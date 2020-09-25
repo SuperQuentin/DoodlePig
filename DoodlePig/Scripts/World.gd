1cf8e6f81ba6d68b2cca31fdea1ce35fd68c3e2c
Hausmann <william.hausmann@cpnv.ch>
2020-09-25 11:39:44 +0200

    Add trapped cloud to generation

--- DoodlePig/Scripts/World.gd
+++ DoodlePig/Scripts/World.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var cloud_platform = preload("res://Scenes/CloudPlatform.tscn")
 var movingcloud_platform = preload("res://Scenes/MovingCloudPlatform.tscn") 
+var trappedcloud_platform= preload("res://Scenes/TrappedCloud.tscn") 
 
 
 var width
@@ -20,6 +21,8 @@ func generateCloud(y):
 		var rand = rand_range(0,100)
 		if rand >= 0 and rand <20 :
 			new_platform = movingcloud_platform.instance()
+		elif rand >20 and rand <=40:
+			new_platform = trappedcloud_platform.instance()
 		else:
 			new_platform = cloud_platform.instance()
 		new_platform.set_position(Vector2(rand_range(-width/2 + offset_x,width/2 - offset_x),y))
